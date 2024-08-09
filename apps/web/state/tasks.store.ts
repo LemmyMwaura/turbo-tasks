52b5a974fac65d03f4d600a6654268e70f12dc21
lemmyMwaura <lemminton@gmail.com>
2024-08-09 23:19:45 +0300

    feat: indent code

--- apps/web/state/tasks.store.ts
+++ apps/web/state/tasks.store.ts
@@ -1,8 +1,5 @@
 import { createStore } from 'zustand/vanilla'
-import { persist, createJSONStorage } from 'zustand/middleware'
 import { DEMOTASKS, Task } from '@repo/ui'
-
-const PREFIX = 'TASK_TRACKER'
 
 const initTaskState = {
   tasks: DEMOTASKS,
@@ -17,41 +14,33 @@ export interface TaskStore {
 }
 
 export const createTaskStore = () => {
-  return createStore<TaskStore>()(
-    
-      (set, get) => ({
-        ...initTaskState,
-        addTask: (task: Task) => {
-          console.log('Adding task:', task)
-          set((state) => ({
-            tasks: [...state.tasks, task],
-          }))
-        },
-        updateTask: (id: string, updatedTask: Partial<Task>) => {
-          console.log('Updating task:', id, updatedTask)
-          set((state) => ({
-            tasks: state.tasks.map((task) =>
-              task.id === id ? { ...task, ...updatedTask } : task
-            ),
-          }))
-        },
-        removeTask: (id: string) => {
-          console.log('Removing task:', id)
-          set((state) => ({
-            tasks: state.tasks.filter((task) => task.id !== id),
-          }))
-        },
-        getTaskById: (id: string) => {
-          const { tasks } = get()
-          const task = tasks.find((task) => task.id === id)
-          console.log('Getting task by id:', id, task)
-          return task
-        },
-      }
-      // {
-      //   name: `${PREFIX}-tasks`,
-      //   storage: createJSONStorage(() => localStorage),
-      // }
-    )
-  )
+  return createStore<TaskStore>()((set, get) => ({
+    ...initTaskState,
+    addTask: (task: Task) => {
+      console.log('Adding task:', task)
+      set((state) => ({
+        tasks: [...state.tasks, task],
+      }))
+    },
+    updateTask: (id: string, updatedTask: Partial<Task>) => {
+      console.log('Updating task:', id, updatedTask)
+      set((state) => ({
+        tasks: state.tasks.map((task) =>
+          task.id === id ? { ...task, ...updatedTask } : task
+        ),
+      }))
+    },
+    removeTask: (id: string) => {
+      console.log('Removing task:', id)
+      set((state) => ({
+        tasks: state.tasks.filter((task) => task.id !== id),
+      }))
+    },
+    getTaskById: (id: string) => {
+      const { tasks } = get()
+      const task = tasks.find((task) => task.id === id)
+      console.log('Getting task by id:', id, task)
+      return task
+    },
+  }))
 }
